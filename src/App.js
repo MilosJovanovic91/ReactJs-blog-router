7c2dbae9c354d9e6a2483a6f8eeed8be532bfed8
Milos <milosjovanovic744@yahoo.com>
2021-12-05 21:15:04 +0100

    currently not working router, have to check why not changing routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Routes, Route, useHistory } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import NewPost from "./components/NewPost";
+import PostPage from "./components/PostPage";
+import About from "./page/About";
+import Home from "./page/Home";
+import MissingPage from "./page/MissingPage";
 
 function App() {
   return (
     <div>
-      <h1>Hello from Milos</h1>
+      <Header />
+      <Nav />
+      <Routes>
+        <Route exact path="/" element={Home} />
+        <Route path="post" element={NewPost} />
+        <Route path="post/:id" element={PostPage} />
+        <Route path="about" element={About} />
+        <Route path="*" element={MissingPage}/>
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/page/MissingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MissingPage = () => {
+    return (
+        <main>
+            <h1>MissingPage</h1>
+        </main>
+    )
+}
+
+export default MissingPage
